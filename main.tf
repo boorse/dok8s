e7c3d8499b5289769a6d890c0e5e99df1a11f510
Gabriel Boorse <gnboorse@gmail.com>
2020-04-14 00:05:11 -0400

    add output

--- main.tf
+++ main.tf
@@ -80,3 +80,8 @@ resource "helm_release" "externaldns" {
     value = "sync"
   }
 }
+
+output "kubeconfig" {
+  value       = digitalocean_kubernetes_cluster.dok8scluster.kube_config[0].raw_config
+  description = "Kubeconfig file contents"
+}
